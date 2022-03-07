8f221e3ebf9644e18f98954fe14c92828cba8998
henrique lauar <henriquelauar2002@gmail.com>
2022-03-07 17:44:50 -0300

    Adding login form using formik and yup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
-import './App.css';
+import "./App.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
 
 function App() {
+
+  const handleClickLogin = (values) => console.log(values);
+  const validationLogin = yup.object().shape({
+    email: yup
+      .string()
+      .email("Não é um e-mail válido")
+      .required("Campo obrigatório"),
+    password: yup
+      .string()
+      .min(8, "A senha deve ter no mínimo 8 dígitos.")
+      .required("Campo obrigatório"),
+  });
   return (
-    <div className="container"></div>
+    <div className="container">
+            <Formik
+        initialValues={{}}
+        onSubmit={handleClickLogin}
+        validationSchema={validationLogin}
+      >
+        <Form className="login-form">
+          <div className="login-form-group">
+            <Field name="email" className="form-field" placeholder="Email" />
+            <ErrorMessage
+              component="span"
+              name="email"
+              className="form-error"
+            />
+          </div>
+
+          <div className="login-form-group">
+            <Field
+              name="password"
+              className="form-field"
+              placeholder="Password"
+            />
+            <ErrorMessage
+              component="span"
+              name="password"
+              className="form-error"
+            />
+          </div>
+
+          <button className="button" type="submit">
+            Login
+          </button>
+        </Form>
+      </Formik>
+    </div>
   );
 }
 
